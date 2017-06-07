b71512498a56070a78094032fc39a44cd722c3f8
Saharat Saengsawang <saharat.earth@gmail.com>
2017-06-07 20:14:11 +0700

    update home page

--- lms/adddata/views.py
+++ lms/adddata/views.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import HttpResponse
+#from .models import Weather
+
+from django.http import JsonResponse
+import json
+
 # Create your views here.
+
+def home(request):
+	return render(request,"home.html")
+
+def show_add_page(request):
+	return render(request,"add_page.html")
--- lms/lms/urls.py
+++ lms/lms/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from adddata import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    #url(r'^add/$', views.show_add_page, name='show_add_page'),
 ]
