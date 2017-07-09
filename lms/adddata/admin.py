087a5b7424d60788a6026ee4a4a790c0ae5a57d4
SothanaV <nai6320@gmail.com>
2017-07-09 22:28:18 +0700

    fixSignupBug

--- lms/adddata/admin.py
+++ lms/adddata/admin.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from views import Post
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+	list_display=[f.name for f in Post._meta.fields]
+	#list_editable=("temp","humi")
+admin.site.register(Post,PostAdmin)
+		
